183a29d6a01aeeb909d18c32f3b37a8c21f527be
shaileshwebdeveloper <shailesh.webdevelopment@gmail.com>
2022-06-06 14:25:07 +0530

    start

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,16 +7,16 @@ const Product = () => {
     <div data-cy={`product-${product.id}`}>
       <h3 data-cy="product-name"></h3>
       <h6 data-cy="product-description"></h6>
-      <button data-cy="product-add-item-to-cart-button"></button>
+      <button data-cy="product-add-item-to-cart-button">Add</button>
       <div>
-        <button data-cy="product-increment-cart-item-count-button"></button>
+        <button data-cy="product-increment-cart-item-count-button">+</button>
         <span data-cy="product-count">
           {
             // Count here from CartItems
           }
         </span>
-        <button data-cy="product-decrement-cart-item-count-button"></button>
-        <button data-cy="product-remove-cart-item-button"></button>
+        <button data-cy="product-decrement-cart-item-count-button">-</button>
+        <button data-cy="product-remove-cart-item-button">Remove</button>
       </div>
     </div>
   );
